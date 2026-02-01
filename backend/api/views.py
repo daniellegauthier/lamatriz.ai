17e8ba0438dd92c959f21e0453296ce044c5ff27
Danielle Gauthier <danielle.gauthier@lamatriz.ai>
2026-02-01 12:57:16 -0500

    Implement analyze API view for POST requests

---
+++ backend/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import AnalyzeRequest
+from core.analyze import analyze
+
+@api_view(["POST"])
+def analyze_view(request):
+    serializer = AnalyzeRequest(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    try:
+        result = analyze(**serializer.validated_data)
+    except ValueError as e:
+        return Response({"error": str(e)}, status=400)
+
+    return Response(result, status=status.HTTP_200_OK)
